18fef3934fab91daba31f580f68fee3cc39b19a6
Jack Dunn <98713017+JD1307@users.noreply.github.com>
2022-04-13 13:34:58 -0400

    Update hw6.sql

--- hw6.sql
+++ hw6.sql
@@ -27,5 +27,24 @@ create table products
     constraint productId
         unique (productId)
 );
+INSERT INTO customers (firstname, lastname, phoneNumber) VALUES ( 'Tom', 'Brady', '4138659842');
+INSERT INTO customers (firstname, lastname, phoneNumber) VALUES ( 'Matt', 'Stafford', '5856217893');
+INSERT INTO customers (firstname, lastname, phoneNumber) VALUES ( 'Joe', 'Burrow', '4012543417');
+INSERT INTO products (productD) VALUES ('Chair');
+INSERT INTO products (productD) VALUES ('Table');
+INSERT INTO products (productD) VALUES ('Paint');
+INSERT INTO products (productD) VALUES ('Brush');
+INSERT INTO orders (customerId, productId) values (1, 3);
+INSERT INTO orders (customerId, productId) values (2, 1);
+INSERT INTO orders (customerId, productId) values (1, 4);
+INSERT INTO orders (customerId, productId) values (3, 2);
+INSERT INTO orders (customerId, productId) values (2, 2);
+INSERT INTO orders (customerId, productId) values (1, 1);
 
+SELECT * from products;
+SELECT * from customers;
+SELECT * from orders;
+SELECT customerId from orders;
+SELECT productId from orders;
+SELECT productD from products;
 
